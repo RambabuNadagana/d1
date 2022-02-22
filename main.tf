e5ebcac3771f512de1cf025b13167937937f2967
RambabuNadagana <94271381+RambabuNadagana@users.noreply.github.com>
2022-02-22 23:01:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,19 @@
-resource "aws_launch_template" "example" {
-  name_prefix   = "example"
-    image_id      = ami-0629230e074c580f2
-    instance_type = t2.micro
-    security_groups    = sg-0bb5391635b3c304es
-    key_name = "jenkins"
-    iam_instance_profile = "ram-s3-role"
+provider "aws" {
+  region = "us-east-2"
 }
-
-resource "aws_autoscaling_group" "example" {
-  availability_zones = ["us-east-2b"]
-  desired_capacity   = 1
-  max_size           = 1
-  min_size           = 1
-
-  mixed_instances_policy {
-    launch_template {
-      launch_template_specification {
-        launch_template_id = aws_launch_template.example.id
-      }
-     }
-  }
+resource "aws_launch_template" "as_conf" {
+  name_prefix   = my_lt
+  image_id      = ami-0629230e074c580f2
+  instance_type = t2.micro
+  security_groups    = sg-0bb5391635b3c304es
+  user_data = "${file("userdata.sh")}"
+}
+resource "aws_autoscaling_group" "bar" {
+  name                 = var.name 
+  depends_on           = ["aws_launch_template.as_conf"]
+  launch_configuration = aws_launch_template.as_conf.name
+  min_size             = 1
+  max_size             = 2
+  desired_capacity     = 1
+  availability_zones = us-east-2a
 }
