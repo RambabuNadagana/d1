54c4122f60db0a37747c739b8e514bdba6a15d17
RambabuNadagana <94271381+RambabuNadagana@users.noreply.github.com>
2022-02-22 23:23:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,30 @@
 provider "aws" {
   region = "us-east-2"
 }
-resource "aws_launch_template" "as_conf" {
-  name_prefix   = dbms
-  image_id      = ami-0629230e074c580f2
-  instance_type = t2.micro
-  security_groups    = sg-0bb5391635b3c304es
-  user_data = "${file("userdata.sh")}"
+resource "aws_instance" "myec2" {
+  depends_on = [aws_db_instance.default]
+  ami           = "ami-0629230e074c580f2"
+  instance_type = "t2.micro"
+  subnet_id   = "subnet-0080182dff1d4159a"
+  key_name = "jenkins"
+  user_data = templatefile("${path.module}/userdata.tftpl", {endpoint = aws_db_instance.default.endpoint,password = aws_db_instance.default.password})
+  iam_instance_profile = "ram-s3-role"
+  security_groups = ["sg-0bb5391635b3c304es"]
+  tags = {
+    Name = "Ec2tf"
+  }
 }
-resource "aws_autoscaling_group" "bar" {
-  name                 = var.name 
-  depends_on           = ["aws_launch_template.as_conf"]
-  launch_template = aws_launch_template.as_conf.name
-  min_size             = 1
-  max_size             = 2
-  desired_capacity     = 1
-  target_group_arns   = [var.target_group_arns]
- availability_zones = us-east-2a
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "cpms"
+  identifier           = "mydb"
+  username             = "admin"
+  password             = "Nareshkumar"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+  publicly_accessible  = true
+  security_group_names = ["sg-0bb5391635b3c304es"]
 }
