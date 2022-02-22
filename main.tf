450166d750872323e36f55485ec90e1fce8c29f2
RambabuNadagana <94271381+RambabuNadagana@users.noreply.github.com>
2022-02-22 22:25:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_db_instance" "default" {
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
-  name                 = "mydb"
+  name                 = "mydb1"
   username             = "foo"
   password             = "foobarbaz"
   parameter_group_name = "default.mysql5.7"
