11aac526e806b7d3a2c331203564cb743f5852b0
RambabuNadagana <94271381+RambabuNadagana@users.noreply.github.com>
2022-02-22 23:11:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-2"
 }
 resource "aws_launch_template" "as_conf" {
-  name_prefix   = my_lt
+  name_prefix   = dbms
   image_id      = ami-0629230e074c580f2
   instance_type = t2.micro
   security_groups    = sg-0bb5391635b3c304es
@@ -11,9 +11,10 @@ resource "aws_launch_template" "as_conf" {
 resource "aws_autoscaling_group" "bar" {
   name                 = var.name 
   depends_on           = ["aws_launch_template.as_conf"]
-  launch_configuration = aws_launch_template.as_conf.name
+  launch_template = aws_launch_template.as_conf.name
   min_size             = 1
   max_size             = 2
   desired_capacity     = 1
-  availability_zones = us-east-2a
+  target_group_arns   = [var.target_group_arns]
+ availability_zones = us-east-2a
 }
