2bf0fab1d03e2babae25dab5e1b54972dd49a22e
RambabuNadagana <94271381+RambabuNadagana@users.noreply.github.com>
2022-02-22 23:43:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,26 @@
-provider "aws" {
-  region = "us-east-2"
-}
-resource "aws_instance" "myec2" {
-  depends_on = [aws_db_instance.default]
-  ami           = "ami-0629230e074c580f2"
-  instance_type = "t2.micro"
-  subnet_id   = "subnet-0080182dff1d4159a"
-  key_name = "jenkins"
-  user_data = templatefile("${path.module}/userdata.tftpl", {endpoint = aws_db_instance.default.endpoint,password = aws_db_instance.default.password})
-  iam_instance_profile = "ram-s3-role"
-  security_groups = ["sg-0bb5391635b3c304es"]
-  tags = {
-    Name = "Ec2tf"
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-0f607673eab7d2eb7"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
   }
-}
-resource "aws_db_instance" "default" {
-  allocated_storage    = 10
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
-  name                 = "cpms"
-  identifier           = "mydb"
-  username             = "admin"
-  password             = "Nareshkumar"
-  parameter_group_name = "default.mysql5.7"
-  skip_final_snapshot  = true
-  publicly_accessible  = true
-  security_group_names = ["sg-0bb5391635b3c304es"]
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
